cb314934784623ed60c9d01f0adfbd3778a30568
Michael James Schock <m@mjschock.com>
2026-01-10 10:19:00 -0800

    Remove apps/ and 3rdparty/ directories reintroduced from main

--- apps/customer-support/backend-ts/debug-stub.ts
+++
@@ -1,9 +0,0 @@
-// debug-stub.ts
-// A simple stub for the 'debug' module to prevent 'require is not defined' errors in Deno.
-
-function debug(_namespace: string): (...args: unknown[]) => void {
-  return (..._args: unknown[]) => {};
-}
-
-export default debug;
-
